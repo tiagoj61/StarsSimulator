40b20fad4607282682f7f262c3ce70b42abf7c83
Tiago Moraes <tiagoj61@gmail.com>
2022-04-12 07:32:58 -0300

    feat: progress indicator and ids

--- src/main/java/tiago/j61/starsimulator/view/SignController.java
+++ src/main/java/tiago/j61/starsimulator/view/SignController.java
@@ -2,10 +2,13 @@ package tiago.j61.starsimulator.view;
 
 import java.io.IOException;
 import java.net.URL;
+import java.security.Provider.Service;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextField;
 import tiago.j61.starsimulator.bo.UsuarioBo;
 import tiago.j61.starsimulator.controller.App;
@@ -15,13 +18,18 @@ public class SignController implements Initializable {
 	TextField usuario_input;
 	@FXML
 	TextField senha_input;
+
+	@FXML
+	ProgressIndicator progress_label;
+	
 	UsuarioBo usuarioBo;
 
 	@FXML
 	private void registerUser() throws IOException {
+		progress_label.setVisible(true);
 		if (usuarioBo.validUser(usuario_input.getText(), senha_input.getText()))
 			App.setRoot("primary");
-		
+		progress_label.setVisible(false);
 	}
 
 	@FXML
@@ -32,7 +40,6 @@ public class SignController implements Initializable {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		usuarioBo = new UsuarioBo();
-		// TODO Auto-generated method stub
-
+	
 	}
 }
